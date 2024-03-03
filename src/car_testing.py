e361c4d8014e70ea7130ed3e8953bc7c1d9ceb4f
DJ200148 <81722638+DJ200148@users.noreply.github.com>
2024-03-02 17:42:17 -0800

    .

--- src/car_testing.py
+++ src/car_testing.py
@@ -6,22 +6,24 @@ con = ConstrolSystem(offset=7)
 try:
     con.turn(-35)
     print("turned left")
-    
+    time.sleep(2)
+
     con.disable_controls()
     print("Stopped")
-    time.sleep(1)
+    time.sleep(2)
     
     con.turn()
-    print("centered")
-    time.sleep(1)
-    
-    con.turn(35)
-    print("turned right")
+    print("tried to go centered")
+    time.sleep(2)
 
     con.enable_controls()
     print("Enabled")
-    time.sleep(1)
+    time.sleep(2)
 
+    con.turn(35)
+    print("turned right")
+    time.sleep(2)
+    
     con.turn()
     print("centered")
 
